9e43ded8422e06625d89b724fb055efe8e3e2658
Ramiel <r4miel05@pm.me>
2025-07-31 23:13:58 +1000

    added TODO re thank you email auto response

--- src/makeup-by-ellie-contact.ts
+++ src/makeup-by-ellie-contact.ts
@@ -77,3 +77,60 @@ export const makeupByEllieContact = async (req: Request<any, any, Body>, res: Re
     res.status(500).json({ error: "Failed to send email." });
   }
 };
+
+// TODO an autmoated thank you email response
+
+// e.g. from netlify functions
+
+// ```ts
+// import formData from "form-data";
+// import Mailgun from "mailgun.js";
+// import Airtable from "airtable";
+
+// const sendThankYouEmail = async ({ email }) => {
+//   console.log('Sending the email');
+//   const { MG_API_KEY: apiKey, MG_DOMAIN: domain } = process.env;
+//   const mailgun = new Mailgun(formData).client({
+//     username: 'api',
+//     key: apiKey,
+//   });
+
+//   const mailData = {
+//     from: `Stefan Judis stefan@${domain}`,
+//     to: email,
+//     subject: 'Thank you for your interest',
+//     text: "I'll come back to you asap!",
+//   };
+
+//   await mailgun.messages.create(domain, mailData);
+// };
+
+// const saveUser = async ({ name, email, message }) => {
+//   const { AT_API_KEY: apiKey, AT_BASE, AT_TABLE } = process.env;
+
+//   Airtable.configure({
+//     apiKey,
+//   });
+
+//   const base = Airtable.base(AT_BASE);
+//   const table = base(AT_TABLE);
+//   await table.create({ name, email, message });
+// };
+
+// export default async (request, context) => {
+//   try {
+//     const data = await request.json();
+
+//     await sendThankYouEmail(data);
+
+//     if (data.receiveUpdates) {
+//       await saveUser(data);
+//     }
+
+//     return Response.json({ message: "Let's become serverless conductors!!!" });
+//   } catch (error) {
+//     console.log(error);
+//     return Response.json({ error: 'Failed sending email' }, { status: 500 });
+//   }
+// };
+// ```
