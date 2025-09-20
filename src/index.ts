382c0a0757b6c63def44d47bf56f2cce7e186bc1
Ramiel <r4miel05@pm.me>
2025-09-20 20:38:48 +1000

    added cors for supported web app

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import { makeupByEllieContact } from "./makeup-by-ellie-contact";
 
 dotenv.config();
 const app = express();
+
+app.use(
+  cors({
+    origin: process.env.ALLOWED_ORIGIN,
+    methods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
